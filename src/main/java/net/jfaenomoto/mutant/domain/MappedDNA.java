c92c8890d3db18815a230e2aa7152a59ad1336dd
Joao Enomoto <jfaenomoto@gmail.com>
2018-10-19 00:02:06 -0300

    - adding persistence to project;

--- src/main/java/net/jfaenomoto/mutant/domain/MappedDNA.java
+++ src/main/java/net/jfaenomoto/mutant/domain/MappedDNA.java
@@ -1,10 +1,31 @@
 package net.jfaenomoto.mutant.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "mapped_dna")
 public class MappedDNA {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "raw_dna")
     private String dna;
 
+    @Column(name = "hash")
+    private String hash;
+
+    @Column(name = "is_mutant")
     private boolean isMutant;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getDna() {
         return dna;
@@ -12,6 +33,14 @@ public class MappedDNA {
 
     public void setDna(String dna) {
         this.dna = dna;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
     }
 
     public boolean isMutant() {
---
+++ src/main/java/net/jfaenomoto/mutant/repository/MappedDNARepository.java
@@ -0,0 +1,12 @@
+package net.jfaenomoto.mutant.repository;
+
+import net.jfaenomoto.mutant.domain.MappedDNA;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MappedDNARepository extends CrudRepository<MappedDNA, Long> {
+
+    MappedDNA findByHash(String hash);
+
+}
